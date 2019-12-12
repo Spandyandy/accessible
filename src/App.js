519159090578345ecc0f4809e9cd923182bdac69
Andy Kim <Junghoo.Kim1@ibm.com>
2019-12-12 16:51:13 -0500

    comment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import ImageMapper from 'react-image-mapper';
 import map from './map.png';
 import './App.css';
 
-
+var SIZE = 8;
 
 var MAP = {
 	name: "my-map",
@@ -41,99 +40,131 @@ var MAP = {
 		{
 			name: "Hogan",
 			shape: "rect",
-			coords: [195, 379, 219, 398],
-			preFillColor: "rgb(255,255,255,0.3)",
-			lineWidth: 1
+			coords: [195*SIZE/5, 379*SIZE/5, 219*SIZE/5, 398*SIZE/5],
+			preFillColor: "rgb(255,255,255,0.1)",
+			strokeColor: "red",
+			lineWidth: 2,
+			details: "2 elevators near the main lounge on first floor"
 		},
 		{
 			name: "SIPA",
 			shape: "rect",
-			coords: [365, 178, 406, 192],
-			preFillColor: "rgb(255,255,255,0.3)",
-			lineWidth: 1
+			coords: [365*SIZE/5, 178*SIZE/5, 406*SIZE/5, 192*SIZE/5],
+			preFillColor: "rgb(255,255,255,0.1)",
+			strokeColor: "red",
+			lineWidth: 2,
+			details: "4 elevators near North entrance, another 4 near South entrance"
 		},
 		{
 			name: "Pupin",
 			shape: "rect",
-			coords: [215, 84, 250, 96],
-			preFillColor: "rgb(255,255,255,0.3)",
-			lineWidth: 1
+			coords: [215*SIZE/5, 84*SIZE/5, 250*SIZE/5, 96*SIZE/5],
+			preFillColor: "rgb(255,255,255,0.1)",
+			strokeColor: "red",
+			lineWidth: 2,
+			details: "Right next to the entrance"
 		},
 		{
 			name: "Kent",
 			shape: "rect",
-			coords: [302, 250, 342, 261],
-			preFillColor: "rgb(255,255,255,0.3)",
-			lineWidth: 1
+			coords: [302*SIZE/5, 250*SIZE/5, 342*SIZE/5, 261*SIZE/5],
+			preFillColor: "rgb(255,255,255,0.1)",
+			strokeColor: "red",
+			lineWidth: 2,
+			details: "Enter the building, turn right, there are 3 elevators"
 		},
 		{
 			name: "Low Library",
 			shape: "rect",
-			coords: [252, 187, 289, 225],
-			lineWidth: 1,
-			preFillColor: "rgba(255, 255, 255, 0.3)",
-			// strokeColor: "#6afd09"
+			coords: [252*SIZE/5, 187*SIZE/5, 289*SIZE/5, 225*SIZE/5],
+			lineWidth: 2,
+			preFillColor: "rgba(255, 255, 255, 0.1)",
+			strokeColor: "red",
+			details: "First floor next to the bathroom"
 		}
 	]
 };
 
-var URL = map;
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      hoveredArea: null, msg: null, moveMsg: null
-    }
+	  hoveredArea: null, clickedArea: null, details: "", stylindDetails: null,
+	  comment: "",
+	  comments: [
+		{
+		  id: 0,
+		  user: "Bonorum Malorum",
+		  content: "Many desktop publishing packages and web page editors now use",
+		  userPic: "https://upload.wikimedia.org/wikipedia/ru/thumb/b/bc/Garfield_the_Cat.svg/1200px-Garfield_the_Cat.svg.png",
+		  publishDate: "2 days ago"
+		},
+		{
+		  id: 1,
+		  user: "Cicero Areals",
+		  content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+		  userPic: "https://static.tgstat.ru/public/images/channels/_0/f0/f0f7f79a275a83bfe8769dfd81d40bb2.jpg",
+		  publishDate: "4 days ago"
+		},
+		{
+		  id: 2,
+		  user: "Hanna Pages",
+		  content: "Lorem Ipsum comes from sectionsof de Finibus Bonorum et Malorum (The Extremes of Good and Evil)",
+		  userPic: "https://vignette.wikia.nocookie.net/versus-compendium/images/0/09/Garfield.png/revision/latest?cb=20181122134939",
+		  publishDate: "1 Week ago"
+		},
+	  ]
+	};
+	this.handleSubmit = this.handleSubmit.bind(this);
+	this.handleChange = this.handleChange.bind(this);
   }
 	load() {
 		this.setState({ msg: "Interact with image !" });
 	}
 	clicked(area) {
 		this.setState({
-			msg: `You clicked on ${area.shape} at coords ${JSON.stringify(
-				area.coords
-			)} !`
+			clickedArea: area,
+			details: area.details,
 		});
 	}
 	clickedOutside(evt) {
-		const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
 		this.setState({
-			msg: `You clicked on the image at coords ${JSON.stringify(coords)} !`
-		});
-	}
-	moveOnImage(evt) {
-		const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
-		this.setState({
-			moveMsg: `You moved on the image at coords ${JSON.stringify(coords)} !`
+			clickedArea: null,
+			details: null
 		});
 	}
 	enterArea(area) {
 		this.setState({
 			hoveredArea: area,
-			msg: `You entered ${area.shape} ${area.name} at coords ${JSON.stringify(
-				area.coords
-			)} !`
 		});
 	}
 	leaveArea(area) {
 		this.setState({
 			hoveredArea: null,
-			msg: `You leaved ${area.shape} ${area.name} at coords ${JSON.stringify(
-				area.coords
-			)} !`
 		});
 	}
-	moveOnArea(area, evt) {
-		const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
-		this.setState({
-			moveMsg: `You moved on ${area.shape} ${
-				area.name
-			} at coords ${JSON.stringify(coords)} !`
-		});
-	}
-
 	getTipPosition(area) {
 		return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
+	}
+	handleSubmit(event) {
+		event.preventDefault();
+		var curr = this.state.comments;
+		curr.unshift({
+			id: curr[curr.length - 1] + 1,
+			user: "anonymous",
+			content: this.state.comment,
+			userPic: "https://devtalk.blender.org/uploads/default/optimized/2X/c/cbd0b1a6345a44b58dda0f6a355eb39ce4e8a56a_2_1380x920.png",
+			publishDate: "just now",
+		})
+		this.setState({
+			comments: curr
+		})
+	}
+	handleChange(event) {
+		event.preventDefault();
+		this.setState({
+			comment: event.target.value
+		})
 	}
 
 	render() {
@@ -142,52 +173,60 @@ class App extends Component {
 				<div className="presenter">
 					<div style={{ position: "relative" }}>
 						<ImageMapper
-							src={URL}
+							src={map}
 							map={MAP}
-							width={500}
+							width={SIZE*100}
 							onLoad={() => this.load()}
 							onClick={area => this.clicked(area)}
 							onMouseEnter={area => this.enterArea(area)}
 							onMouseLeave={area => this.leaveArea(area)}
-							onMouseMove={(area, _, evt) => this.moveOnArea(area, evt)}
 							onImageClick={evt => this.clickedOutside(evt)}
-							onImageMouseMove={evt => this.moveOnImage(evt)}
 							lineWidth={4}
 							strokeColor={"white"}
 						/>
 						{this.state.hoveredArea && (
 							<span
-								className="tooltip"
+								className="hover-tooltip"
 								style={{ ...this.getTipPosition(this.state.hoveredArea) }}
 							>
 								{this.state.hoveredArea && this.state.hoveredArea.name}
 							</span>
 						)}
 					</div>
-					<pre className="message">
-						{this.state.msg ? this.state.msg : null}
-					</pre>
-					<pre>{this.state.moveMsg ? this.state.moveMsg : null}</pre>
 				</div>
 				<div className="source">
-					<h2>Accessibility Information :</h2>
-					Building Name : &nbsp;
-					<span
-						onClick={() =>
-							this.setState({ codeDetails: !this.state.codeDetails })
+					<h2>Accessibility Information</h2>
+					Building Name : {this.state.clickedArea ? this.state.clickedArea.name:null}
+					  <br/>
+					  <br/>
+					Details : <p>{this.state.details}</p>
+					<div class="comment-container">
+						<h2>Add A Comment Below</h2>
+						<div class="form-group">
+							<textarea class="form-control status-box" rows="3" name="c" onChange={this.handleChange} placeholder="Enter your comment here..."></textarea>
+						</div>
+						<div class="button-group pull-right">
+						<button class="btn btn-primary" onClick={this.handleSubmit}>Post</button>
+						</div>
+					</div>
+
+					<ul class="posts">
+					<div className="commentBox">
+						{this.state.comments.map((comment) => {
+							return <div className="comment">
+								<img src={comment.userPic} className="commentPic" alt="user Pic"/>
+								<div className="commentBody">
+								<div className="commentHeader">
+									<span className="commentAuthor">{comment.user}</span>
+									<span className="publishDate">{comment.publishDate}</span>
+								</div>
+								<span className="commentContent">{comment.content}</span>
+								</div>
+							</div>
+							})
 						}
-					>
-						{this.state.codeDetails ? "[-]" : "[+]"}
-          </span>
-          <br/>
-					Details : &nbsp;
-					<span
-						onClick={() =>
-							this.setState({ stylindDetails: !this.state.stylindDetails })
-						}
-					>
-						{this.state.stylindDetails ? "[-]" : "[+]"}
-					</span>
+					</div>
+					</ul>
 				</div>
 			</div>
 		);
